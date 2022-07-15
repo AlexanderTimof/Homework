23b885325beb47f83582938d95666aa0901c1cfa
AlexanderTimof <Cokpat1989@gmail.com>
2022-07-15 15:50:48 +0500

    нашёл второй способ решения первой задачи

---
+++ src/Lesson2/Homework_adv.java
@@ -0,0 +1,39 @@
+package Lesson2;
+
+public class Homework_adv {
+    public static void main(String[] args) {
+
+        //Задача 1
+
+        String str = "234";
+        String smtx = "some_text";
+        int i = Integer.parseInt (str);
+        int countLetters = smtx.length();
+        int answer = i + countLetters;
+
+        System.out.println(answer);
+
+        //Задача 2
+
+        int a = 3;
+        int b = 5;
+        int c = a + b;
+
+        System.out.println(Math.pow(c , 2 ));
+
+        //Задача 3
+
+        int[] numbers1 = new int[]{1, 2, 5, 7, 10};
+        int[] numbers2 = new int[]{2, 3, 2, 17, 15};
+
+
+
+
+
+
+
+
+
+
+    }
+}
